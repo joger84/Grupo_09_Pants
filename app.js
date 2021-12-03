cfe04897a63f2b569b56d4229da9816f5558dace
jorge-sas <joger_84@hotmail.com>
2021-12-02 19:38:29 -0500

    (Ale) New se inicia proyecto final

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.resolve(__dirname,'public')))
+
+app.listen(3000,()=> console.log('Servidor arriba!'))
+
+app.get('/', (req,res) => res.sendFile(path.resolve(__dirname,'views/index.html')))
