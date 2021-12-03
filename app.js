22271a970d88ca55b458b62f831c898dd732e767
elgrantribi <rossilisandro@hotmail.com>
2021-12-02 22:21:33 -0300

    (Li) Hice el registro

--- app.js
+++ app.js
@@ -7,3 +7,7 @@ app.use(express.static(path.resolve(__dirname,'public')))
 app.listen(3000,()=> console.log('Servidor arriba!'))
 
 app.get('/', (req,res) => res.sendFile(path.resolve(__dirname,'views/index.html')))
+
+
+
+app.get('/register', (req,res) => res.sendFile(path.resolve(__dirname,'views/register.html')))
