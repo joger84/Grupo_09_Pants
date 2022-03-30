a1a61cca324b7b48400a5caf7f7a9ff7446f063d
SofiDN <sofia_dinanno@hotmail.com>
2022-03-30 00:26:16 -0300

    (sofi) modificaciones en User controller, rutas , register.ejs.

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -33,6 +33,7 @@ const controllerUser = {
     store: async(req, res) => {
         const postUser = {
             ...req.body,
+            password: bcrypt.hashSync(req.body.password, 10),
             image:req.file.filename,
             
 
@@ -50,7 +51,7 @@ const controllerUser = {
     },
     //Ruta por GET proceso de modificacion de profile
     editProfile: function(req,res) {
-        User.findBbyPk(req.params.id)
+        User.findByPk(req.params.id)
             .then(function(userEdit){
               res.render('./users/editProfile', {userEdit:userEdit});
 
@@ -76,90 +77,26 @@ const controllerUser = {
        })
        // redirecciono a la pagina donde estaba el usuario con su id
        // ver si no es conveniente cambiar todo para que sea por eMail
-       res.redirect("./users/editProfile" + req.params.id)
+       res.redirect("/user/editProfile/" + req.params.id)
     },
-
     
-    //*****************CODIGO UTILIZADO PARA CUANDO TRABAJABAMOS CON DB JSON ***********
-    
-    
-    /*store: (req,res) =>{
-        const resultValidation = validationResult(req);
-        if(resultValidation.errors.length){
-            return res.render('./users/register', {errors: resultValidation.mapped(),oldDate:req.body})
-        }
-        const generateID = () => {
-            const lastUser = users[users.length - 1];
-            // 2. Obtenemos el ID de ese último usuario
-            const lastID = lastUser.id;
-            // 3. Retornamos ese último ID incrementado en 1
-            return lastID + 1;
-        }
-       //const dataBody = req.body;        
-       //delete dataBody.confirmClave
-       //pasar en el PUSH 'dataBody' en vez de req.body
-        
-        let userRegister = users.find( oneUser => oneUser.email=== req.body.email);
-        if (userRegister){
-            return res.render('./users/register', {
-                errors: {
-                    email:{
-                        msg: 'Email ya registrado'
-                    }
-                },
-               oldDate:req.body
-            });
-        }
-        
-        if (users.length) {
-            users.push({
-                id: generateID(),
-                fullName: req.body.fullName,
-                usuario: req.body.usuario,
-                email: req.body.email,
-                clave: bcrypt.hashSync(req.body.clave, 10),
-                image: req.file.filename,
-                fecha: req.body.fecha,
-                direccion: req.body.direccion,
-                genero: req.body.genero
-            })
-        } else {
-            users.push({
-                id: 1,                    // Revisar    
-                fullName: req.body.fullName,
-                usuario: req.body.usuario,
-                email: req.body.email,
-                clave: bcrypt.hashSync(req.body.clave, 10),
-                image: req.file.filename,
-                fecha: req.body.fecha,
-                direccion: req.body.direccion,
-                genero: req.body.genero
-            })
-        }
-        
-        fs.writeFileSync(usersPath, JSON.stringify(users, null, ' '));
-        
-        return res.redirect('/user/login');
-    },*/
-    
-
-
+   
     // Ruta por POST proceso de login
     loginProcess: async (req, res) => {
-       const validacionLogin = validationResult(req)
+        const validacionLogin = validationResult(req)
        if(validacionLogin.errors.length){
            return res.render('./users/login', {errors: validacionLogin.mapped(),oldDate:req.body})
         }
         
         // 1. validar que el usuario que quiere loguearse este en la dataBase 
-
+        
         const userToLogin= await User.findOne({
             where: {
                 eMail: req.body.eMail
             }
         });
         console.log(userToLogin)
-           
+        
         // 2. validar que la contraseña sea la misma que la guardada
         if (userToLogin){
             const passwordCorrect = bcrypt.compareSync(req.body.password, userToLogin.password);
@@ -192,7 +129,73 @@ const controllerUser = {
         req.session.destroy();
         return res.redirect("/");
     }
-
+    
 }
 
 module.exports = controllerUser;
+
+
+
+
+
+//*****************CODIGO UTILIZADO PARA CUANDO TRABAJABAMOS CON DB JSON ***********
+
+
+/*store: (req,res) =>{
+    const resultValidation = validationResult(req);
+    if(resultValidation.errors.length){
+        return res.render('./users/register', {errors: resultValidation.mapped(),oldDate:req.body})
+    }
+    const generateID = () => {
+        const lastUser = users[users.length - 1];
+        // 2. Obtenemos el ID de ese último usuario
+        const lastID = lastUser.id;
+        // 3. Retornamos ese último ID incrementado en 1
+        return lastID + 1;
+    }
+   //const dataBody = req.body;        
+   //delete dataBody.confirmClave
+   //pasar en el PUSH 'dataBody' en vez de req.body
+    
+    let userRegister = users.find( oneUser => oneUser.email=== req.body.email);
+    if (userRegister){
+        return res.render('./users/register', {
+            errors: {
+                email:{
+                    msg: 'Email ya registrado'
+                }
+            },
+           oldDate:req.body
+        });
+    }
+    
+    if (users.length) {
+        users.push({
+            id: generateID(),
+            fullName: req.body.fullName,
+            usuario: req.body.usuario,
+            email: req.body.email,
+            clave: bcrypt.hashSync(req.body.clave, 10),
+            image: req.file.filename,
+            fecha: req.body.fecha,
+            direccion: req.body.direccion,
+            genero: req.body.genero
+        })
+    } else {
+        users.push({
+            id: 1,                    // Revisar    
+            fullName: req.body.fullName,
+            usuario: req.body.usuario,
+            email: req.body.email,
+            clave: bcrypt.hashSync(req.body.clave, 10),
+            image: req.file.filename,
+            fecha: req.body.fecha,
+            direccion: req.body.direccion,
+            genero: req.body.genero
+        })
+    }
+    
+    fs.writeFileSync(usersPath, JSON.stringify(users, null, ' '));
+    
+    return res.redirect('/user/login');
+},*/
