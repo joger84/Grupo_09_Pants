33a5bfd59c2becb87a925f5760e9bf1dcc824c34
elgrantribi <rossilisandro@hotmail.com>
2022-04-01 22:13:20 -0300

    (Li) Validación nombrecompleto register user desde el front

--- public/users/jsFront/validations.js
+++ public/users/jsFront/validations.js
@@ -1,20 +1,21 @@
 window.addEventListener("load", function (){
-    let form = document.querySelector("formCreate");
+    let form = document.querySelector(".formCreate");
+    console.log ("conectar")
     form.addEventListener("submit", function(e){
         let errors = [];
-        let fullName = document.querySelector("fullName");
+        let fullName = document.querySelector(".fullName");
         if (fullName.value == ""){
             errors.push("El campo Nombre no puede estar vacío")
         }else if(fullName.value.length <= 3)
             errors.push("El campo Nombre debe tener mas de 3 caracteres")
+            if(errors.length >0){
+                e.preventDefault();
+                let arrayErrors = document.querySelector("div.arrayErrors");
+                for(let i=0; i< errors.length; i++){
+                    arrayErrors.innerHTML="<li>"+errors[i]+"</li>"
+                }
+            }
     })
-    if(errors.length >0){
-        e.preventDefault();
-        let arrayErrors = document.querySelector("div.arrayErrors");
-        for(let i=0; i< errors.length; i++){
-            arrayErrors.innerHTML+="<li>"+errors[i]+"</li>"
-        }
-    }
     }
 )
 
