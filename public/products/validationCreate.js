bc9644c64f0eb8cf59c58a4f1cb0a834f764fd93
jorge-sas <joger_84@hotmail.com>
2022-04-10 11:13:50 -0500

    (Ale) Subo validaciones del front de Create product

--- public/products/validationCreate.js
+++ public/products/validationCreate.js
@@ -2,50 +2,48 @@ window.addEventListener("load", function (){
     let form = document.querySelector(".formCreate");
     console.log ("conectado al formCreate de Products")
         form.addEventListener("submit", function(e){
-         
+            let model = document.querySelector(".productName");
+            let descrip = document.querySelector(".description");
+            let cant = document.querySelector(".cantidad");
+            let img = document.querySelector('.image')
+            let price = document.querySelector(".price");
             let errors = [];
-                let model = document.querySelector(".productName");
-                let mensajeErrorProducName = document.querySelector(".errorProductName");
-                console.log(model)
-                if (model.value == ""){
-                    errors.push("Debes completar el campo Modelo")
-                    mensajeErrorProducName.innerHTML="Debes completar el campo Modelo"
-                } else if(model.value.length <= 5){
-                    errors.push("El campo Modelo debe tener mas de 5 caracteres");
-                    mensajeErrorProducName.innerHTML="El campo Modelo debe tener mas de 5 caracteres"
+                
+            if (model.value == ""){
+                errors.push("Debes completar el campo Modelo")
+            } else if(model.value.length <= 5){
+                errors.push("El campo Modelo debe tener mas de 5 caracteres");
+            }
+            
+            if (descrip.value == ""){
+                errors.push("Debes completar el campo Descripcion")
+            } else if(descrip.value.length <= 20){
+                errors.push("El campo Descripcion debe tener mas de 20 caracteres");
+            }
+            
+            console.log(cant)
+            if (cant.value == ""){
+                errors.push("Debes completar una cantidad de productos")
+            }
+
+            if(img.value == ''){
+                errors.push("Por favor ingrese una imagen");
+            } 
+            
+            if (price.value == ""){
+                errors.push("Debes completar un precio de venta")
+            }
+
+            //revisamos la cantidad de msj de error y seteamos en la vista
+            if(errors.length >0){
+                e.preventDefault();
+                let productsArrayErrors = document.querySelectorAll(".productsArrayErrors");
+                let errorPrueba=[...productsArrayErrors]
+                // console.log(errorPrueba)
+                for(let i=0; i< errors.length; i++){
+                    errorPrueba[i].innerHTML= errors[i];
                 }
-
-                let descrip = document.querySelector(".description");
-                console.log(descrip)
-                if (descrip.value == ""){
-                    errors.push("Debes completar el campo Descripcion")
-                } else if(descrip.value.length <= 20){
-                    errors.push("El campo Descripcion debe tener mas de 20 caracteres");
-                }
-                let cant = document.querySelector(".cantidad");
-                console.log(cant)
-                if (cant.value == ""){
-                    errors.push("Debes completar una cantidad de productos")
-                }
-
-                let price = document.querySelector(".price");
-                    console.log(price)
-                    if (price.value == ""){
-                        errors.push("Debes completar un precio de venta")
-                    }
-
-
-                    if(errors.length >0){
-                        e.preventDefault();
-                        let productsArrayErrors = document.querySelectorAll(".productsArrayErrors");
-                        let errorPrueba=[...productsArrayErrors]
-                        // console.log(errorPrueba)
-                        for(let i=0; i< errors.length; i++){
-                            errorPrueba[i].innerHTML= errors[i]
-                        }
-                    }
-           console.log(errors) 
-
+            }
         })
     }            
 )
