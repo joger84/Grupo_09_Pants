494b7be4f83aae6a6aed40ee72dc661d5487de47
SofiDN <sofia_dinanno@hotmail.com>
2022-04-10 18:59:16 -0300

    (sofi)validation EditProduct FRONT. arch retro

--- public/products/validationEdit.js
+++ public/products/validationEdit.js
@@ -1,45 +1,91 @@
-/*window.addEventListener("load", function (){
-    let form = document.querySelector(".formEdit");
-    console.log ("conectado al formEdit de Products")
-        form.addEventListener("submit", function(e){
-                let errors = [];
-                let nombre = document.querySelector(".productName");
-                console.log(model)
-                if (nombre.value == ""){
-                    errors.push("Debes completar el campo Nombre del Modelo")
-                } else if(nombre.value.length <= 5)
-                    errors.push("El campo Modelo debe tener mas de 5 caracteres");
-                    
-                let descrip = document.querySelector(".description");
-                console.log(descrip)
-                if (descrip.value == ""){
-                    errors.push("Debes completar el campo Descripcion")
-                } else if(descrip.value.length <= 20)
-                    errors.push("El campo Descripcion debe tener mas de 20 caracteres");
+window.addEventListener('load', function(e){
 
-                let cant = document.querySelector(".cantidad");
-                console.log(cant)
-                if (cant.value == ""){
-                    errors.push("Debes completar una cantidad de productos")
-                }
 
-                let price = document.querySelector(".price");
-                    console.log(price)
-                    if (price.value == ""){
-                        errors.push("Debes completar un precio de venta")
-                    }
-                            
+const formEditProduct = document.querySelector(".formEdit");
+console.log("Estas dentro del Form Edit Product")
 
-                    if(errors.length >0){
-                        e.preventDefault();
-                        let productsArrayErrors = document.querySelector("div.productsEditArrayErrors");
-                        for(let i=0; i< errors.length; i++){
-                            productsArrayErrors.innerHTML="<li>"+errors[i]+"</li>"
-                        }
-                    }
-            
 
-        })
-    }            
-)
-*/
+
+const productModelField = document.querySelector(".productName")
+const productDescriptionField = document.querySelector(".description"); 
+const productQuantityField = document.querySelector(".cantidad"); 
+const productimgField = document.querySelector('#image')
+const productcategoriaField = document.querySelector("#categoria"); 
+const productColorField = document.querySelector("#colores"); 
+const productSizeField = document.querySelector("#tall"); 
+const productPriceField = document.querySelector(".price"); 
+console.log(productModelField, productDescriptionField, productQuantityField, productimgField, productcategoriaField, productColorField, productSizeField, productPriceField)
+
+
+errorArray=0;
+const validationEmptyField = (e) =>{
+    const field = e.target //target indica que elemento fue el que disparo el evento
+    const productspanTagError = field.nextElementSibling; // capturo a la etiqueta que esta despues del field 
+    console.log(productspanTagError)
+    if (field.value.trim() === ""){
+        field.classList.add("is-invalid")
+        productspanTagError.innerText = `El campo ${field.name} es obligatorio`;
+        productspanTagError.classList.add("invalid-feedback");
+        
+        errorArray++;
+    } else {
+        field.classList.remove("is-invalid")
+        field.classList.add("is-valid")
+        productspanTagError.innerText = "";
+        productspanTagError.classList.remove("invalid-feedback");
+    };
+    
+    }
+
+//pasamos la f(x) validationEmptyField para que ninguno de los campos pueda estar vacio
+productModelField.addEventListener('blur', validationEmptyField);
+productModelField.addEventListener('input', (e)=>{
+    const field = e.target;
+    const model = field.value;
+    const productspanTagError = field.nextElementSibling;
+    if (model.length<5){
+        field.classList.add("is-invalid")
+        productspanTagError.innerText = `El campo debe contener al menos 5 digitos`;
+        productspanTagError.classList.add("invalid-feedback");
+    } else {
+        field.classList.remove("is-invalid")
+        field.classList.add("is-valid")
+        productspanTagError.innerText = "";
+        productspanTagError.classList.remove("invalid-feedback");
+    };
+});
+
+
+productDescriptionField.addEventListener('blur', validationEmptyField);
+productDescriptionField.addEventListener('input', (e)=>{
+    const field = e.target;
+    const description = field.value;
+    const productspanTagError = field.nextElementSibling;
+    if (description.length < 20){
+        field.classList.add("is-invalid")
+        productspanTagError.innerText = `El campo debe contener al menos 20 digitos`;
+        productspanTagError.classList.add("invalid-feedback");
+    } else {
+        field.classList.remove("is-invalid")
+        field.classList.add("is-valid")
+        productspanTagError.innerText = "";
+        productspanTagError.classList.remove("invalid-feedback");
+    };
+});
+
+
+productQuantityField.addEventListener('blur', validationEmptyField);
+productimgField.addEventListener('blur', validationEmptyField);
+productcategoriaField.addEventListener('blur', validationEmptyField);
+productColorField.addEventListener('blur', validationEmptyField);
+productSizeField.addEventListener('blur', validationEmptyField);
+productPriceField.addEventListener('blur', validationEmptyField);
+
+formEditProduct.addEventListener("submit", function(e){
+if (errorArray<0) {
+    e.preventDefault();
+    
+}
+})
+
+})
