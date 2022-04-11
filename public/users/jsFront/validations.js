8a1e972da3512df0626be7a384846289efb73527
jdecheverria <jorgedanielecheverria@hotmail.com>
2022-04-11 10:56:42 -0300

    (jor) Agrego validaciones del form register

--- public/users/jsFront/validations.js
+++ public/users/jsFront/validations.js
@@ -1,68 +1,142 @@
-/*window.addEventListener("load", function (){
-    let form = document.querySelector(".formCreate");
-    console.log ("conectar")
-    form.addEventListener("submit", function(e){
-        let errors = [];
-        let fullName = document.querySelector(".fullName");
-        if (fullName.value == ""){
-            errors.push("El campo Nombre no puede estar vacío")
-        }else if(fullName.value.length <= 3)
-            errors.push("El campo Nombre debe tener mas de 3 caracteres")
-            if(errors.length >0){
-                e.preventDefault();
-                let arrayErrors = document.querySelector("div.arrayErrors");
-                for(let i=0; i< errors.length; i++){
-                    arrayErrors.innerHTML="<li>"+errors[i]+"</li>"
-                }
-            }
-    })
-    }
-)*/
-const form = document.querySelector(".formCreate");
-console.log("Estas en Form Register")
-const fullNameField = document.querySelector("[name=fullName]");
-const userField = document.querySelector("[name=user]");
-const emailField = document.querySelector("[name=eMail]");
-const passwordField = document.querySelector("[name=password]");
+window.addEventListener('load', function(e){
 
-const validateEmptyField = (e) => {
-	const field = e.target;
-	const spanTagError = field.nextElementSibling;
-	if (field.value.trim() === "") {
-		field.classList.add("is-invalid");
-		spanTagError.innerHTML = `El campo ${field.name} es obligatorio`;
-        spanTagError.classList.add("invalid-feedback");
-        field.classList.remove("is-valid");
-	} else {
-		field.classList.remove("is-invalid");
-		field.classList.add("is-valid");
-		spanTagError.innerHTML = "";
-		spanTagError.classList.remove("invalid-feedback");
-	}
-}
+    const form = document.querySelector(".formCreate");
+    console.log("Estas en Form Register")
 
-fullNameField.addEventListener("blur", validateEmptyField);
-userField.addEventListener("blur", validateEmptyField);
-emailField.addEventListener("blur", validateEmptyField);
-passwordField.addEventListener("blur", validateEmptyField);
-
- form.addEventListener("submit", (e)=>{
-     e.preventDefault();
-    const expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
-    const fullNameValue = fullNameField.value.length
-    const spanTagError = field.nextElementSibling;
-      console.log(fullNameValue);
-      if(fullNameValue <2){
-          spanTagError.innerText = `El Nombre debe tener más de 2 caracteres`;
-          spanTagError.classList.add("invalid-feedback");
-          field.classList.add("is-invalid");
-        }
-        if(expReg.test(emailField.value)){
-            console.log(emailField.value);
+    const fullNameField = document.querySelector("[name=fullName]");
+    const userField = document.querySelector("[name=user]");
+    const emailField = document.querySelector("[name=eMail]");
+    const passwordField = document.querySelector("[name=password]");
+    const imgField = document.querySelector('[name=avatarImage]');
+    console.log(fullNameField, userField, emailField, passwordField, imgField)
+    
+    errorRegisterArray=0;
+    
+    const validateEmptyField = (e) => {
+        const field = e.target;
+        const spanTagError = field.nextElementSibling;
+        if (field.value.trim() === "") {
             field.classList.add("is-invalid");
+            spanTagError.innerHTML = `El campo ${field.name} es obligatorio`;
             spanTagError.classList.add("invalid-feedback");
             field.classList.remove("is-valid");
+            errorRegisterArray++
+            
+        } else {
+            field.classList.remove("is-invalid");
+            field.classList.add("is-valid");
+            spanTagError.innerHTML = "";
+            spanTagError.classList.remove("invalid-feedback");
         }
-    })
+    }
+    //pasamos la f(x) validationEmptyField para que ninguno de los campos pueda estar vacio
+    fullNameField.addEventListener("blur", validateEmptyField);
+    fullNameField.addEventListener('input', (e)=>{
+        const field = e.target;
+        const fullName = field.value;
+        const spanTagError = field.nextElementSibling;
+        if (fullName.length<2){
+            field.classList.add("is-invalid")
+            spanTagError.innerText = `El campo debe contener al menos 2 digitos`;
+            spanTagError.classList.add("invalid-feedback");
+            field.classList.remove("is-valid");
+
+            errorRegisterArray++
+        } else {
+            field.classList.remove("is-invalid")
+            field.classList.add("is-valid")
+            spanTagError.innerText = "";
+            spanTagError.classList.remove("invalid-feedback");
+        };
+    });
+
+    userField.addEventListener("blur", validateEmptyField);
+    userField.addEventListener("input", (e)=>{
+        const field = e.target;
+        const userName = field.value;
+        const spanTagError = field.nextElementSibling;
+          console.log();
+          if (userName.length<2){
+            field.classList.add("is-invalid")
+            spanTagError.innerText = `El campo debe contener al menos 5 digitos`;
+            spanTagError.classList.add("invalid-feedback");
+            field.classList.remove("is-valid");
+
+            errorRegisterArray++
+        } else {
+            field.classList.remove("is-invalid")
+            field.classList.add("is-valid")
+            spanTagError.innerText = "";
+            spanTagError.classList.remove("invalid-feedback");
+        };
+    });
+    emailField.addEventListener("blur", validateEmptyField);
+    emailField.addEventListener("input", (e)=>{
+        const field = e.target;
+        const email = field.value;
+        const spanTagError = field.nextElementSibling;
+          console.log();
+          if (email.length<2){
+            field.classList.add("is-invalid")
+            spanTagError.innerText = `El campo debe contener al menos 5 digitos`;
+            spanTagError.classList.add("invalid-feedback");
+            field.classList.remove("is-valid");
+
+            errorRegisterArray++
+        } else {
+            field.classList.remove("is-invalid")
+            field.classList.add("is-valid")
+            spanTagError.innerText = "";
+            spanTagError.classList.remove("invalid-feedback");
+        };
+        })
+    passwordField.addEventListener("blur", validateEmptyField);
+    passwordField.addEventListener("input", (e)=>{
+        const field = e.target;
+        const password = field.value;
+        const spanTagError = field.nextElementSibling;
+          console.log();
+          if (password.length<8){
+            field.classList.add("is-invalid")
+            spanTagError.innerText = `El campo debe contener al menos 5 digitos`;
+            spanTagError.classList.add("invalid-feedback");
+            field.classList.remove("is-valid");
+
+            errorRegisterArray++
+        } else {
+            field.classList.remove("is-invalid")
+            field.classList.add("is-valid")
+            spanTagError.innerText = "";
+            spanTagError.classList.remove("invalid-feedback");
+        };
+        })
+        
+     imgField.addEventListener('blur', validateEmptyField);
+     imgField.addEventListener('blur', (e) =>{
+    const acceptedExtensions = [".jpg", ".png", ".jpeg", ".gif"];
+    const field = e.target;
+    const image = field.value;
+    const spanTagError = field.nextElementSibling;
+    if (image !== acceptedExtensions){
+        field.classList.add("is-invalid")
+      spanTagError.innerText = "Debes cargar una extension valida .jpg, .png, .jpeg, .gif";
+      spanTagError.classList.add("invalid-feedback");
+      field.classList.add("is-valid")
+  } else {
+      field.classList.remove("is-invalid")
+      field.classList.add("is-valid")
+      spanTagError.innerText = "";
+      spanTagError.classList.remove("invalid-feedback");
+  };
+
+});
+console.log(errorRegisterArray)
+        form.addEventListener("submit", function(e){
+            if (errorRegisterArray>0) {
+                e.preventDefault();
+            }
+            })
 
 
+    
+})
